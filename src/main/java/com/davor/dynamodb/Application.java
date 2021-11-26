43ed5c3e009752ac0a8d0db64553c6675f4aaffc
Jorge Lara <larajorge11@gmail.com>
2021-11-26 18:11:13 -0500

    feat(Add dynamodb dependency)

--- src/main/java/com/davor/dynamodb/Application.java
+++ src/main/java/com/davor/dynamodb/Application.java
@@ -1,4 +1,4 @@
-package com.davor.dynamodb.davor.dynamodb;
+package com.davor.dynamodb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/davor/dynamodb/ApplicationTests.java
+++ src/test/java/com/davor/dynamodb/ApplicationTests.java
@@ -1,4 +1,4 @@
-package com.davor.dynamodb.davor.dynamodb;
+package com.davor.dynamodb;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
